5b72aa2b7db7ffdef861a1e6879174889d5b6808
vladik-Didyk <64746247+brandonrock919@users.noreply.github.com>
2025-01-31 14:15:55 -0500

    Initial commit

---
+++ library/components/buttons/src/lib/buttons.tsx
@@ -0,0 +1,11 @@
+import styles from './buttons.module.css';
+
+export function Buttons() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Buttons!</h1>
+    </div>
+  );
+}
+
+export default Buttons;
---
+++ library/components/buttons/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/library/components/buttons',
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  test: {
+    setupFiles: ['./src/test-setup.ts'],
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/library/components/buttons',
+      provider: 'v8',
+    },
+  },
+});
